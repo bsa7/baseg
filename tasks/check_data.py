9ff57b4695de94f630ae3bf340064e03249d19f5
DrWhats <hudorojckov.leva@gmail.com>
2023-03-27 23:16:55 +0500

    BS-22 Убедиться в корректности сырых данных датасета

--- tasks/check_data.py
+++ tasks/check_data.py
@@ -9,9 +9,13 @@ argument_parser = ArgumentParser()
 input_file_name = argument_parser.argument_safe('input_file',
                                                 'Ошибка! Вы должны указать имя файла с данными о пополнении баланса!')
 
+# ---------Раскомментировать, чтобы брать только часть строк------------
 pf = ParquetFile(input_file_name)
 first_rows = next(pf.iter_batches(batch_size=10000000))  # Здесь можно задать количество первых n строк из паркета
 data = patb.from_batches([first_rows]).to_pandas()
+
+# ---------Раскомментировать, чтобы брать все строки------------
+# data = pd.read_parquet(input_file_name)
 
 # Проверяем, есть ли дублирующиеся строки
 duplicateRows = data[data.duplicated()]
