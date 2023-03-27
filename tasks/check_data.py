8ad72cbc582c482b902e2efc7cf7570add2cbfd3
DrWhats <hudorojckov.leva@gmail.com>
2023-03-27 22:12:16 +0500

    BS-022 Убедиться в корректности сырых данных датасета

---
+++ tasks/check_data.py
@@ -0,0 +1,32 @@
+''' Этот файл проверяет корректность данных в датасете '''
+import pandas as pd
+import pyarrow.parquet as pq
+from app.lib.argument_parser import ArgumentParser
+from pyarrow.parquet import ParquetFile
+from pyarrow import Table as patb
+
+argument_parser = ArgumentParser()
+input_file_name = argument_parser.argument_safe('input_file',
+                                                'Ошибка! Вы должны указать имя файла с данными о пополнении баланса!')
+
+pf = ParquetFile(input_file_name)
+first_rows = next(pf.iter_batches(batch_size=10000000))  # Здесь можно задать количество первых n строк из паркета
+data = patb.from_batches([first_rows]).to_pandas()
+
+# Проверяем, есть ли дублирующиеся строки
+duplicateRows = data[data.duplicated()]
+print("Дублированных строк:", duplicateRows.shape[0])
+
+# Выводим есть ли нулевые значения
+print("Строки с нулчевыми значениями есть? ", data.isnull().values.any())
+
+# Переводим дату из строки в дату
+data["rep_date_dt"] = pd.to_datetime(data["rep_date"])
+
+# Отделяем год от даты
+data["year"] = data["rep_date_dt"].apply(lambda x: x.year)
+
+# Группируем по году и считаем кол-во
+data.groupby(data["year"]).count()
+
+print(data.groupby(data["year"]).count())
