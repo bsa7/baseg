264cb64265d15eaf5c24928924b86afa6de02385
Sergio Belevskij <bbelevskij@gmail.com>
2023-03-30 17:53:38 +0300

    BS-21 EDA - анализ и визуализация

--- docker/run_task.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-set -e
-
-docker-compose run --rm app bash -l -c "python -m $@"
--- tasks/eda.py
+++ tasks/eda.py
@@ -4,6 +4,7 @@
 # Пример запуска:
 # python -m tasks.eda input_file=../baseg-shared/data/wallet_urfu.parquet.gzip
 
+import matplotlib.pyplot as plt
 from app.lib.argument_parser import ArgumentParser
 from app.lib.service_factory import ServiceFactory
 from app.lib.parquet import Parquet
@@ -17,46 +18,24 @@ spark = ServiceFactory().spark('SparkWithSQL')
 df = Parquet().read_to_spark_df(input_file_name, spark)
 df.createOrReplaceTempView('replenishments')
 
-# Данный пример находит даты самого первого и самого последнего пополнений
-# result = spark.sql(r"""
-#   SELECT
-#     max(rep_date) AS max_date,
-#     min(rep_date) AS min_date
-#   FROM replenishments
-# """)
-
-# print('Даты самого последнего и самого первого пополнений')
-# result.show(1)
-
-# Данный пример находит наибольшую и наименьшую суммы пополнений
-# result = spark.sql(r"""
-#   SELECT
-#     max(monetary) AS max_replenishment,
-#     min(monetary) AS min_replenishment
-#   FROM replenishments
-# """)
-
-
-# print('Наибольшая и наименьшая суммы пополнений:')
-# result.show(1)
-
 # Данный пример аггрегирует пополнения помесячно, для каждого клиента
 # Для подсветки синтаксиса SQL в VSCode для python можно использовать расширение python-string-sql
 result = spark.sql(f"""--beginsql
   SELECT
     partner AS client_id,
     CONCAT(CAST(YEAR(rep_date) AS STRING), '-', RIGHT(CONCAT('00', CAST(MONTH(rep_date) AS STRING)), 2)) AS rep_month,
-    SUM(monetary) AS monetary_per_month
+    COUNT(monetary) AS operation_count
   FROM replenishments
-  WHERE
-    rep_date >= '2017-03-01'
-    AND rep_date < '2018-03-01'
+   WHERE
+     rep_date >= '2016-01-01'
+     AND rep_date < '2023-12-31'
   GROUP BY
-    rep_month,
-    client_id
+    client_id,
+    rep_month
   ORDER BY
-    monetary_per_month DESC
+    client_id,
+    rep_month
 --endsql""")
 
-print('Аггрегированные суммы пополнений:')
-result.show(10)
+print('Количества операций:')
+result.show(100)
